64e0739bf93924fafbce31d9e6ea73ff0cea627c
TheSlipper <korneldomeradzki1@gmail.com>
2020-09-16 22:52:16 +0200

    Created views, templates and redirects for most of the ui

--- ui/urls.py
+++ ui/urls.py
@@ -2,5 +2,33 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard')
+    # Dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    # Devices
+    path('devices/manage/list', views.devices_list, name='devices_list'),
+    path('devices/manage/requests', views.devices_requests, name='devices_requests'),
+    path('devices/manage/api_inspection', views.devices_api_inspection, name='devices_api_inspection'),
+
+    path('devices/settings/general_settings', views.devices_general_settings, name='devices_general_settings'),
+    path('devices/settings/api_settings', views.devices_api_settings, name='devices_api_settings'),
+
+    # Cameras
+    path('cameras/manage/list', views.cameras_list, name='cameras_list'),
+    path('cameras/manage/detection_areas', views.cameras_detection_areas, name='cameras_detection_areas'),
+    path('cameras/manage/streaming_requests', views.cameras_streaming_requests, name='cameras_streaming_requests'),
+    path('cameras/manage/livestream', views.cameras_livestream, name='cameras_livestream'),
+
+    path('cameras/manage/general_settings', views.cameras_general_settings, name='cameras_general_settings'),
+    path('cameras/manage/streaming_settings', views.cameras_streaming_settings, name='cameras_streaming_settings'),
+
+    # Users
+    path('users/manage/list', views.users_list, name='users_list'),
+    path('users/manage/roles', views.users_role_list, name='users_role_list'),
+    path('users/manage/bans', views.users_ban_list, name='users_ban_list'),
+
+
+    # Misc.
+    path('misc/quick_start', views.misc_quick_start, name='misc_quick_start'),
+    path('misc/help', views.misc_help, name='misc_help'),
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,5 +1,74 @@
 from django.shortcuts import render
 
 
+# Dashboard
 def dashboard(request):
     return render(request, 'ui/dashboard.html')
+
+
+# Devices views
+def devices_list(request):
+    return render(request, 'ui/devices/list.html')
+
+
+def devices_requests(request):
+    return render(request, 'ui/devices/device_requests.html')
+
+
+def devices_api_inspection(request):
+    return render(request, 'ui/devices/api_inspection.html')
+
+
+def devices_general_settings(request):
+    return render(request, 'ui/devices/general_settings.html')
+
+
+def devices_api_settings(request):
+    return render(request, 'ui/devices/api_settings.html')
+
+
+# Cameras views
+def cameras_list(request):
+    return render(request, 'ui/cameras/list.html')
+
+
+def cameras_detection_areas(request):
+    return render(request, 'ui/cameras/detection_areas.html')
+
+
+def cameras_streaming_requests(request):
+    return render(request, 'ui/cameras/requests.html')
+
+
+def cameras_livestream(request):
+    return render(request, 'ui/cameras/livestream.html')
+
+
+def cameras_general_settings(request):
+    return render(request, 'ui/cameras/general_settings.html')
+
+
+def cameras_streaming_settings(request):
+    return render(request, 'ui/cameras/streaming_settings.html')
+
+
+# Users
+def users_list(request):
+    return render(request, 'ui/users/user_list.html')
+
+
+def users_role_list(request):
+    return render(request, 'ui/users/role_list.html')
+
+
+def users_ban_list(request):
+    return render(request, 'ui/users/bans_list.html')
+
+
+# Misc.
+def misc_quick_start(request):
+    return render(request, 'ui/misc/quick_start.html')
+
+
+def misc_help(request):
+    return render(request, 'ui/misc/help.html')
