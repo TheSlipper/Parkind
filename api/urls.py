5fe177536dd977ae1346bdd0885dbf69dc68141b
TheSlipper <korneldomeradzki1@gmail.com>
2020-10-02 11:15:43 +0200

    Added a basic CRUD for devices entity in api

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('device/list', views.device_list, name='device_list'),
+    path('device/detail/<int:id>/', views.device_detail, name='device_detail'),
+    path('device/create', views.device_create, name='device_create'),
+    path('device/update/<int:id>/', views.device_update, name='device_update'),
+    path('device/delete/<int:id>/', views.device_delete, name='device_delete')
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,51 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Device
+from .serializers import DeviceSerializer
+
+
+@api_view(['GET'])
+def device_list(request):
+    devices = Device.objects.all()
+    serializer = DeviceSerializer(devices, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def device_detail(request, id):
+    devices = Device.objects.get(id=id)
+    serializer = DeviceSerializer(devices, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def device_create(request):
+    serializer = DeviceSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return HttpResponse(status=201)
+
+    return HttpResponse(status=400)
+
+
+@api_view(['PUT'])
+def device_update(request, id):
+    device = Device.objects.get(id=id)
+    serializer = DeviceSerializer(instance=device, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return HttpResponse(status=200)
+
+    return HttpResponse(status=400)
+
+
+@api_view(['DELETE'])
+def device_delete(request, id):
+    device = Device.objects.get(id=id)
+    device.delete()
+    return HttpResponse(status=200)
