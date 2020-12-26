2d04754dd46546d6f0f1a156931b7a89d9a1add8
TheSlipper <korneldomeradzki1@gmail.com>
2020-12-26 22:14:29 +0100

    Added time to logging

--- ParkindClient/log.go
+++ ParkindClient/log.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const (
 	colorDefault = "\033[0m"
@@ -13,6 +16,8 @@ const (
 )
 
 func log(a []string) {
+	t := time.Now()
+	fmt.Printf("[%s]: ", t.Format("02/01/2006 15:04:05"))
 	for _, foo := range a {
 		fmt.Print(foo)
 	}
